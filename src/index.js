c68559c519046f1ac9168dea637e40c4279c58f4
Dao Xuan Quy <quydxhe176351@fpt.edu.vn>
2024-03-16 16:36:11 +0700

    FER202 SP24 Template

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./pages"
+import ModalsProvider from "./context/ModalsContext"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+])
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+    <React.StrictMode>
+        <ModalsProvider>
+            <RouterProvider router={router} />
+        </ModalsProvider>
+    </React.StrictMode>
+)
